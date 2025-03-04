6b90e97253cc2b2e5a15163f8752c055ae470cb3
Sam Kim <samuelkim9241@gmail.com>
2025-03-04 14:44:08 -0500

    frontend updates

--- frontend/capstone/src/app/app.component.ts
+++ frontend/capstone/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {LoginComponent} from './login/login.component'
+import {GraphComponent} from './graph/graph.component'
+import { Plotly } from 'angular-plotly.js';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,GraphComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
