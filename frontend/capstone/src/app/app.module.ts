e7b07b258c2f2febe546f26354821d7b1d3b6baf
Sam Kim <samuelkim9241@gmail.com>
2025-03-04 06:36:37 -0500

    app.module

---
+++ frontend/capstone/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlotlyModule } from 'angular-plotly.js';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlotlyModule
+  ]
+})
+export class AppModule { }
