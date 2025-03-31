7f5ab9d5039eaad88944892d899c6eabb93e292d
Sam Kim <samuelkim9241@gmail.com>
2025-03-31 10:48:43 -0400

    resnet,vgg,inception NN api calls

--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,26 +1,56 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-toolbar',
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
-  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
-
-  FileInput(event: Event) {
-    event.preventDefault(); 
-    this.fileInput.nativeElement.click(); 
-  }
-
-  onFileSelected(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const file = input.files[0];
-      console.log('Selected file:', file.name);
-      alert('Selected file: ' + file.name);
+  constructor(private NNAPI: ApiService) { }
+  
+  ResNet50() {
+    
+    this.NNAPI.ResNet().subscribe({
+      next: (response) => {
+        console.log(response);
+        alert('ResNet feature extraction successful');
+      },
+      error: (error) => {
+        console.log(error);
+        alert('ResNet feature extraction not successful');
+      }
+    });
+    }
+ VGG16() {
+    this.NNAPI.VGG().subscribe({
+      next: (response) => {
+        console.log(response);
+        alert('VGG feature extraction successful');
+      },
+      error: (error) => {
+        console.log(error);
+        alert('VGG feature extraction not successful');
+      }
+    });
+    }
+  InceptionV3() {
+    
+    this.NNAPI.Inception().subscribe({
+      next: (response) => {
+        console.log(response);
+        alert('Inception feature extraction successful');
+      },
+      error: (error) => {
+        console.log(error);
+        alert('Inception feature extraction not successful');
+      }
+    });
     }
   }
-}
+  
+
+
