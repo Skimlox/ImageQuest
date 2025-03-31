fb7f3e566fc485e1d0500c238981c6a382fc919d
Sam Kim <samuelkim9241@gmail.com>
2025-03-31 00:50:48 -0400

    add remaining endpoints (vgg and inception)

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -5,11 +5,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private url = 'http://127.0.0.1:5000/resnet'
+  private url_resnet = 'http://127.0.0.1:5000/resnet'
+  private url_vgg = 'http://127.0.0.1:5000/vgg'
+  private url_inception = 'http://127.0.0.1:5000/inception'
   constructor(private http: HttpClient) { }
   
   ResNet(): Observable<any> {
-    return this.http.post<any>(this.url, {});
+    return this.http.post<any>(this.url_resnet, {});
   }
-
+  VGG(): Observable<any> {
+    return this.http.post<any>(this.url_vgg, {});
+  }
+  Inception(): Observable<any> {
+    return this.http.post<any>(this.url_inception, {});
+  }
 }
