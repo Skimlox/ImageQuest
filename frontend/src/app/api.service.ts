359d5e01098c97bad83b7b36a9c0a9eb0df14fc2
Sam Kim <samuelkim9241@gmail.com>
2025-03-31 00:45:59 -0400

    remove backend tests and add resnet endpoint

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -5,7 +5,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private url = 'http://127.0.0.1:5000'
+  private url = 'http://127.0.0.1:5000/resnet'
+  constructor(private http: HttpClient) { }
   
+  ResNet(): Observable<any> {
+    return this.http.post<any>(this.url, {});
+  }
 
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {GraphComponent} from './graph/graph.component';
 import {ToolbarComponent} from './toolbar/toolbar.component';
-import { ParametersComponent } from './parameters/parameters.component';
 import { ApiService } from './api.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,GraphComponent,ToolbarComponent,ParametersComponent],
+  imports: [RouterOutlet,RouterLink,GraphComponent,ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,11 +14,5 @@ export class AppComponent {
   title = 'capstone';
   message: string = '';
 
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit() {
-    this.apiService.getHelloMessage().subscribe((data) => {
-      this.message = data.message;
-    });
-  }
+  
 }
