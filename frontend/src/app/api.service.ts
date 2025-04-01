97141c7b3c0c2d47ce947981692944991664560f
Sam Kim <samuelkim9241@gmail.com>
2025-04-01 00:24:27 -0400

    api methods for pca/tsne

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -8,6 +8,13 @@ export class ApiService {
   private url_resnet = 'http://127.0.0.1:5000/resnet'
   private url_vgg = 'http://127.0.0.1:5000/vgg'
   private url_inception = 'http://127.0.0.1:5000/inception'
+  private url_resnet_pca = 'http://127.0.0.1:5000/resnetpca'
+  private url_vgg_pca = 'http://127.0.0.1:5000/vggpca'
+  private url_inception_pca = 'http://127.0.0.1:5000/inceptionpca'
+  private url_resnet_tsne = 'http://127.0.0.1:5000/resnettsne'
+  private url_vgg_tsne = 'http://127.0.0.1:5000/vggtsne'
+  private url_inception_tsne = 'http://127.0.0.1:5000/inceptiontsne'
+
   constructor(private http: HttpClient) { }
   
   ResNet(): Observable<any> {
@@ -18,5 +25,25 @@ export class ApiService {
   }
   Inception(): Observable<any> {
     return this.http.post<any>(this.url_inception, {});
+  }
+
+  ResNetPCA(): Observable<any> {
+    return this.http.post<any>(this.url_resnet_pca, {});
+  }
+  VGGPCA(): Observable<any> {
+    return this.http.post<any>(this.url_vgg_pca, {});
+  }
+  InceptionPCA(): Observable<any> {
+    return this.http.post<any>(this.url_inception_pca, {});
+  }
+
+  ResNetTSNE(): Observable<any> {
+    return this.http.post<any>(this.url_resnet_tsne, {});
+  }
+  VGGTSNE(): Observable<any> {
+    return this.http.post<any>(this.url_vgg_tsne, {});
+  }
+  InceptionTSNE(): Observable<any> {
+    return this.http.post<any>(this.url_inception_tsne, {});
   }
 }
