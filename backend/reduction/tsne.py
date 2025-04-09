2220701002ade7705308b701b820ec63544df8b2
Sam Kim <samuelkim9241@gmail.com>
2025-04-08 23:33:53 -0400

    PCA/tsne updated so that image urls alongside (x,y) coordinates are sent to frontend for plotting and image thumbnails

--- backend/reduction/tsne.py
+++ backend/reduction/tsne.py
@@ -13,37 +13,89 @@ def resnet_tsne():
     blob = bucket.blob('feature_vectors/resnet_features.pkl')
     file = blob.download_as_string()
     resnet_features= pickle.loads(file)
-   
+
+    features_arr = []
+    image_urls = []
+    for x in resnet_features:
+    
+        features = x['features'][0]
+        features_arr.append(features)
+        url = x['url']
+        image_urls.append(url)
+
     tsne = TSNE(n_components=2)
-    data_resnet = tsne.fit_transform(resnet_features)
+    data_resnet = tsne.fit_transform(features_arr)
+
+    result = []
+    for i in range(len(data_resnet)):
+        result.append({
+        'x': data_resnet[i][0],
+        'y': data_resnet[i][1],
+        'image_url': image_urls[i]
+    })
     
     if blob.exists():
         blob.delete()
-
-    return data_resnet
-
+    
+    return result
+    
 def vgg_tsne():
     blob = bucket.blob('feature_vectors/vgg_features.pkl')
     file = blob.download_as_string()
     vgg_features= pickle.loads(file)
    
-    tsne = TSNE(n_components=2)
-    data_vgg = tsne.fit_transform(vgg_features)
+    features_arr = []
+    image_urls = []
+    for x in vgg_features:
     
+        features = x['features'][0]
+        features_arr.append(features)
+        url = x['url']
+        image_urls.append(url)
+
+    tsne = TSNE(n_components=2)
+    data_vgg = tsne.fit_transform(features_arr)
+
+    result = []
+    for i in range(len(data_vgg)):
+        result.append({
+        'x': data_vgg[i][0],
+        'y': data_vgg[i][1],
+        'image_url': image_urls[i]
+    })
+    
+
     if blob.exists():
         blob.delete()
-    
-    return data_vgg
 
+    return result
 def inception_tsne():
     blob = bucket.blob('feature_vectors/inception_features.pkl')
     file = blob.download_as_string()
     inception_features= pickle.loads(file)
+   
+    features_arr = []
+    image_urls = []
+    for x in inception_features:
     
+        features = x['features'][0]
+        features_arr.append(features)
+        url = x['url']
+        image_urls.append(url)
+
     tsne = TSNE(n_components=2)
-    data_inception = tsne.fit_transform(inception_features)
+    data_inception = tsne.fit_transform(features_arr)
+
+    result = []
+    for i in range(len(data_inception)):
+        result.append({
+        'x': data_inception[i][0],
+        'y': data_inception[i][1],
+        'image_url': image_urls[i]
+    })
     
+
     if blob.exists():
         blob.delete()
-    
-    return data_inception
+
+    return result
