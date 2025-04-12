8ec6333358601654f2fa98d1dcc7557627c8e1c4
Sam Kim <samuelkim9241@gmail.com>
2025-04-11 23:05:34 -0400

    combine api endpoints for pca/tsne

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -5,45 +5,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private url_resnet = 'http://127.0.0.1:5000/resnet'
-  private url_vgg = 'http://127.0.0.1:5000/vgg'
-  private url_inception = 'http://127.0.0.1:5000/inception'
-  private url_resnet_pca = 'http://127.0.0.1:5000/resnetpca'
-  private url_vgg_pca = 'http://127.0.0.1:5000/vggpca'
-  private url_inception_pca = 'http://127.0.0.1:5000/inceptionpca'
-  private url_resnet_tsne = 'http://127.0.0.1:5000/resnettsne'
-  private url_vgg_tsne = 'http://127.0.0.1:5000/vggtsne'
-  private url_inception_tsne = 'http://127.0.0.1:5000/inceptiontsne'
+
+  private url = 'http://127.0.0.1:5000'
 
   constructor(private http: HttpClient) { }
+  DeclareModel(model: string): Observable<any> {
+    return this.http.post<any>(`${this.url}/setmodel`, {model});
+  }
   
-  ResNet(): Observable<any> {
-    return this.http.post<any>(this.url_resnet, {});
+  FeatureExtraction(model: string): Observable<any> {
+    return this.http.post(`${this.url}/${model}`, {});
   }
-  VGG(): Observable<any> {
-    return this.http.post<any>(this.url_vgg, {});
-  }
-  Inception(): Observable<any> {
-    return this.http.post<any>(this.url_inception, {});
-  }
-
-  ResNetPCA(): Observable<any> {
-    return this.http.post<any>(this.url_resnet_pca, {});
-  }
-  VGGPCA(): Observable<any> {
-    return this.http.post<any>(this.url_vgg_pca, {});
-  }
-  InceptionPCA(): Observable<any> {
-    return this.http.post<any>(this.url_inception_pca, {});
-  }
-
-  ResNetTSNE(): Observable<any> {
-    return this.http.post<any>(this.url_resnet_tsne, {});
-  }
-  VGGTSNE(): Observable<any> {
-    return this.http.post<any>(this.url_vgg_tsne, {});
-  }
-  InceptionTSNE(): Observable<any> {
-    return this.http.post<any>(this.url_inception_tsne, {});
+  
+  DimensionalityReduction(method: 'pca' | 'tsne'): Observable<any> {
+    return this.http.post<any>(`${this.url}/${method}`, {});
   }
 }
